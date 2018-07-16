c7f5a7e6533140d3b257945b3bad829776259688
Intengine <studio@intengine.pl>
2018-07-16 13:15:04 +0200

    Add Tool Strip Buttons
    Add GUI, set database.

---
+++ DownloadManager/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace DownloadManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsAddUrl = new System.Windows.Forms.ToolStripButton();
+            this.tsRemove = new System.Windows.Forms.ToolStripButton();
+            this.tsSettings = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.AutoSize = false;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsAddUrl,
+            this.toolStripSeparator1,
+            this.tsRemove,
+            this.toolStripSeparator2,
+            this.tsSettings});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 80);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(0, 80);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(800, 370);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 80);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 80);
+            // 
+            // tsAddUrl
+            // 
+            this.tsAddUrl.AutoSize = false;
+            this.tsAddUrl.Image = global::DownloadManager.Properties.Resources.iconAddUrl;
+            this.tsAddUrl.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsAddUrl.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsAddUrl.Name = "tsAddUrl";
+            this.tsAddUrl.Size = new System.Drawing.Size(75, 75);
+            this.tsAddUrl.Text = "Add URL";
+            this.tsAddUrl.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // tsRemove
+            // 
+            this.tsRemove.AutoSize = false;
+            this.tsRemove.Image = global::DownloadManager.Properties.Resources.iconRemove;
+            this.tsRemove.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsRemove.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsRemove.Name = "tsRemove";
+            this.tsRemove.Size = new System.Drawing.Size(75, 75);
+            this.tsRemove.Text = "Remove";
+            this.tsRemove.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // tsSettings
+            // 
+            this.tsSettings.AutoSize = false;
+            this.tsSettings.Image = global::DownloadManager.Properties.Resources.iconSettings;
+            this.tsSettings.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsSettings.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsSettings.Name = "tsSettings";
+            this.tsSettings.Size = new System.Drawing.Size(75, 75);
+            this.tsSettings.Text = "Settings";
+            this.tsSettings.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "DownloadManager";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ToolStripButton tsAddUrl;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton tsRemove;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton tsSettings;
+    }
+}
+
